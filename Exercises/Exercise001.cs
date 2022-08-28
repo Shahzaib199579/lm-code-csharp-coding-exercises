b47d38932d3d9c3c46c3ecad55ad3c8c5f6ca276
shahzaib199579 <shahzaib199579@gmail.com>
2022-08-29 00:35:32 +0100

    Completed Exercise 1.

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Exercises.Models;
 
 namespace Exercises
@@ -9,35 +10,56 @@ namespace Exercises
         public string CapitalizeWord(string word)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (String.Equals(word, null) || String.Equals(word, String.Empty) || String.Equals(word, " "))
+                return word;
+
+            return word[0].ToString().ToUpper() + word.Substring(1);
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return firstName.Substring(0, 1) + "." + lastName.Substring(0, 1);
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (originalPrice < 0)
+                throw new ArgumentException("Price cannot be negative. Please enter a valid price.");
+            else if (vatRate < 0)
+                throw new ArgumentException("VAT cannot be negative. Please enter a valid VAT.");
+            else if (vatRate == 0)
+                return originalPrice;
 
             // NB: Look in Exercise001Tests.cs
             //     There is a test with commented out assertions.
             //     For an extra challenge, uncomment those assertions and make that test pass too.
+            return Math.Round(originalPrice + (originalPrice * (vatRate / 100)), 2);
         }
 
         public string Reverse(string sentence)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (String.Equals(sentence, " ") || String.Equals(sentence, null))
+                return sentence;
+
+            List<char> charactersList = new List<char>();
+            charactersList.AddRange(sentence);
+            charactersList.Reverse();
+
+            return String.Join("", charactersList);
         }
 
         public int CountLinuxUsers(List<User> users)
         {
+            if (users == null)
+                return 0;
+            else if (users.Count() == 0)
+                return 0;
+
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return users.Where(x => String.Equals(x.Type, "Linux")).Count();
         }
     }
 }
