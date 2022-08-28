b2e17f1602cfc52e1022aed7a98e477759b5307e
shahzaib199579 <shahzaib199579@gmail.com>
2022-08-29 00:47:53 +0100

    Completed Excercise 2.

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -1,26 +1,30 @@
-﻿using System;
+﻿using Exercises.Models;
+using System;
 namespace Exercises
 {
     public class Exercise002
     {
         // 👉 Remember to delete all unnecessary comments (like this one) when you're done! 
 
-        public bool IsFromManchester(/* Person person */)
+        public bool IsFromManchester(Person person)
         {
             // 👉 DON'T FORGET to uncomment the tests in Exercise002Tests.cs 
             // Uncomment the parameter above and delete this comment
 
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (person == null)
+                return false;
+
+            return person.City.ToLower().Equals("manchester");
         }
 
-        public bool CanWatchFilm(/*Person person, int ageLimit*/)
+        public bool CanWatchFilm(Person person, int ageLimit)
         {
             // 👉 DON'T FORGET to uncomment the tests in Exercise002Tests.cs 
             // Uncomment the parameters above and delete this comment
 
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return person.Age >= ageLimit;
         }
     }
 }
